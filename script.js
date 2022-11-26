d52a7711e5b7e5519847e5b25755e08ca2163f7d
Ganesh M. Naik <96367168+ganeshhnr72@users.noreply.github.com>
2022-11-26 16:41:56 +0530

    https

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   var url =
-    "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+    "https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
   $.get(url, (value) => {
     var tableData = document.getElementById("table-data");
     let table = $("<table>");
