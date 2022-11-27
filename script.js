9fd4a334a7e05f559e1f965d10f581b6a4256867
Ganesh M. Naik <96367168+ganeshhnr72@users.noreply.github.com>
2022-11-27 10:48:21 +0530

    code cleaning

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
   var url =
-    "https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+    "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
   $.get(url, (value) => {
     var tableData = document.getElementById("table-data");
     let table = $("<table>");
     let tbody = $("<tbody>");
     for (let i = 0; i < value.length; i++) {
       let tr = $("<tr>").attr("class", "data-row");
-      tr.attr("id", i);
+      // tr.attr("id", i);
       let td1 = $("<td>").attr("class", "column1").html(value[i].id);
       let td2 = $("<td>").attr("class", "column2").html(value[i].firstName);
       let td3 = $("<td>").attr("class", "column3").html(value[i].lastName);
@@ -23,7 +23,7 @@ $(document).ready(() => {
     table.append(tbody);
     table.attr("id", "myTable");
     tableData.append(table[0]);
-    console.log(table[0]);
+    // console.log(table[0]);
 
     let tdCount = document.getElementsByTagName("tr");
     for (let k = 0; k < tdCount.length; k++) {
@@ -47,32 +47,15 @@ $(document).ready(() => {
   });
 
   $("#search-box").keyup(() => {
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, table, tr, i, txtValue;
     input = document.getElementById("search-box");
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      let td1 = tr[i].getElementsByTagName("td")[1];
-      let td2 = tr[i].getElementsByTagName("td")[2];
-      let td3 = tr[i].getElementsByTagName("td")[3];
-      let td4 = tr[i].getElementsByTagName("td")[4];
-
-      // console.log(td);
       if (true) {
-        txtValue = td.textContent || td.innerText;
-        let txtValue1 = td1.textContent || td1.innerText;
-        let txtValue2 = td2.textContent || td2.innerText;
-        let txtValue3 = td3.textContent || td3.innerText;
-        let txtValue4 = td4.textContent || td4.innerText;
-        if (
-          txtValue.toUpperCase().indexOf(filter) > -1 ||
-          txtValue1.toUpperCase().indexOf(filter) > -1 ||
-          txtValue2.toUpperCase().indexOf(filter) > -1 ||
-          txtValue3.toUpperCase().indexOf(filter) > -1 ||
-          txtValue4.toUpperCase().indexOf(filter) > -1
-        ) {
+        txtValue = tr[i].textContent || tr[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
         } else {
           tr[i].style.display = "none";
